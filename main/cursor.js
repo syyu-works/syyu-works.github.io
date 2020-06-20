d9006a08f6333376517609f72c16d187d928bd90
syyu-works <67189288+syyu-works@users.noreply.github.com>
2020-06-20 19:23:56 +0900

    Add files via upload

---
+++ main/cursor.js
@@ -0,0 +1,4 @@
+const cursor = document.querySelector('.cursor')
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;")
+})
